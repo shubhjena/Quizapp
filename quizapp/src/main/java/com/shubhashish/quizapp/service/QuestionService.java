51667b6360a596def5daa7d5e322e5c014cdca21
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-08-06 23:50:18 +0530

    feat: add service layer

---
+++ quizapp/src/main/java/com/shubhashish/quizapp/service/QuestionService.java
@@ -0,0 +1,35 @@
+package com.shubhashish.quizapp.service;
+
+import com.shubhashish.quizapp.Question;
+import com.shubhashish.quizapp.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionService {
+    @Autowired
+    QuestionDao questionDao;
+
+    public ResponseEntity<List<Question>> getAllQuestions(){
+        try{
+            return  new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    public List<Question> getQuestionsByCategory(String category) {
+        return questionDao.findByCategory(category);
+    }
+
+    public String addQuestion(Question question) {
+        questionDao.save(question);
+        return "success";
+    }
+}
