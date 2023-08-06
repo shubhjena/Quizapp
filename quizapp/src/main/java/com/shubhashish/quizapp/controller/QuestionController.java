dc7963b75a275e2588796e50dc6e2072dd7ae64f
shubhjena <117462852+shubhjena@users.noreply.github.com>
2023-08-06 23:49:35 +0530

    feat: add question controller

---
+++ quizapp/src/main/java/com/shubhashish/quizapp/controller/QuestionController.java
@@ -0,0 +1,34 @@
+package com.shubhashish.quizapp.controller;
+
+
+import com.shubhashish.quizapp.Question;
+import com.shubhashish.quizapp.service.QuestionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("question")
+class QuestionController {
+    @Autowired
+    QuestionService questionService;
+
+    @GetMapping("allQuestions")
+    public ResponseEntity<List<Question>> getAllQuestions(){
+        return questionService.getAllQuestions();
+    }
+
+    @GetMapping("category/{category}")
+    public List<Question> getQuestionsByCategory(@PathVariable String category){
+    return questionService.getQuestionsByCategory(category);
+    }
+
+    @PostMapping("add")
+    public String addQuestion(@RequestBody Question question){
+        return questionService.addQuestion(question);
+    }
+}
